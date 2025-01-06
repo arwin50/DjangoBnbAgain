5fd529255c27228ae57bd0023922b9a282581f7e
powtzulist <delasanarwin@gmail.com>
2025-01-06 19:50:33 +0800

    [setup] login functionality

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -2,31 +2,67 @@
 import { CustomButton } from "../forms/CustomButton";
 import { Modal } from "./Modal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import { useRouter } from "next/navigation";
+import { handleLogin } from "@/app/lib/actions";
+import { apiService } from "@/app/services/apiService";
+import { useState } from "react";
 
 export const LoginModal = () => {
+  const router = useRouter();
   const LoginModal = useLoginModal();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<string[]>([]);
+
+  const submitLogin = async () => {
+    const formData = {
+      email: email,
+      password: password,
+    };
+    const response = await apiService.post(
+      "/api/auth/login/",
+      JSON.stringify(formData)
+    );
+
+    if (response.access) {
+      handleLogin(response.user.pk, response.access, response.refresh);
+
+      LoginModal.close();
+
+      router.push("/");
+    } else {
+      setErrors(response.non_field_errors);
+    }
+  };
+
   const content = (
     <>
-      <form className="space-y-4">
+      <form action={submitLogin} className="space-y-4">
         <input
+          onChange={(e) => setEmail(e.target.value)}
           type="email"
           placeholder="Your e-mail address"
           className="w-full h-[54px] border px-4 border-gray-300 rounded-xl"
         />
         <input
+          onChange={(e) => setPassword(e.target.value)}
           type="password"
           placeholder="Your password"
           className="w-full h-[54px] border px-4 border-gray-300 rounded-xl"
         />
 
-        <div className="p-5 bg-airbnb text-white rounded-xl opacity-80 ">
-          error
-        </div>
+        {errors.map((error, index) => {
+          return (
+            <div
+              key={`error_${index}`}
+              className="p-5 bg-airbnb text-white rounded-xl opacity-80 "
+            >
+              {error}
+            </div>
+          );
+        })}
 
-        <CustomButton
-          label="Submit"
-          onClick={() => console.log("skibidisigma")}
-        />
+        <CustomButton label="Submit" onClick={submitLogin} />
       </form>
     </>
   );
