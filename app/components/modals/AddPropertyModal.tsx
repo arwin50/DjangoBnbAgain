5cfdf0505399c08e75560dd504c58f6975168e1d
powtzulist <delasanarwin@gmail.com>
2025-01-07 17:45:42 +0800

    [setup] api for adding properties

--- app/components/modals/AddPropertyModal.tsx
+++ app/components/modals/AddPropertyModal.tsx
@@ -1,20 +1,78 @@
 "use client";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { Modal } from "./Modal";
 import useAddPropertyModal from "@/app/hooks/usePropertyModal";
 import { CustomButton } from "../forms/CustomButton";
 import { Categories } from "../addproperty/Categories";
+import { SelectCountry, SelectCountryValue } from "../forms/SelectCountry";
+import Image from "next/image";
+import { apiService } from "@/app/services/apiService";
+import { useRouter } from "next/navigation";
 
 export const AddPropertyModal = () => {
   const [currentStep, setCurrentStep] = useState(1);
   const [dataCategory, setDataCategory] = useState("");
   const [dataTitle, setDataTitle] = useState("");
   const [dataDescription, setDataDecsription] = useState("");
+  const [dataPrice, setDataPrice] = useState("");
+  const [dataBedrooms, setDataBedrooms] = useState("");
+  const [dataBathrooms, setDataBathrooms] = useState("");
+  const [dataGuests, setDataGuests] = useState("");
+  const [dataCountry, setDataCountry] = useState<SelectCountryValue>();
+  const [dataImage, setDataImage] = useState<File | null>(null);
 
   const addPropertyModal = useAddPropertyModal();
 
+  const router = useRouter();
+
   const setCategory = (category: string) => {
     setDataCategory(category);
+  };
+
+  const setImage = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const tmpImage = event.target.files[0];
+      setDataImage(tmpImage);
+    }
+  };
+
+  const submitForm = async () => {
+    console.log("skibidi");
+
+    if (
+      dataCategory &&
+      dataTitle &&
+      dataDescription &&
+      dataPrice &&
+      dataCountry &&
+      dataImage
+    ) {
+      const formData = new FormData();
+      formData.append("category", dataCategory);
+      formData.append("title", dataTitle);
+      formData.append("description", dataDescription);
+      formData.append("price_per_night", dataPrice);
+      formData.append("bedrooms", dataBedrooms);
+      formData.append("bathrooms", dataBathrooms);
+      formData.append("guests", dataGuests);
+      formData.append("country", dataCountry.label);
+      formData.append("country_code", dataCountry.value);
+      formData.append("image", dataImage);
+
+      const response = await apiService.post(
+        "/api/properties/create/",
+        formData
+      );
+
+      if (response.success) {
+        console.log("skibidi is sigma");
+
+        router.push("/");
+        addPropertyModal.close();
+      } else {
+        console.log("Errorskibidi");
+      }
+    }
   };
 
   const content = (
@@ -59,8 +117,102 @@ export const AddPropertyModal = () => {
           />
           <CustomButton label="Next" onClick={() => setCurrentStep(3)} />
         </>
+      ) : currentStep == 3 ? (
+        <>
+          <h2 className="mb-6 text-2xl">Details</h2>
+
+          <div className="pt-3 pb-6 space-y-4">
+            <div className="flex flex-col space-y-2">
+              <label>Price per night</label>
+              <input
+                type="number"
+                value={dataPrice}
+                onChange={(e) => setDataPrice(e.target.value)}
+                className="w-full p-4 border border-gray-600 rounded-xl"
+              />
+            </div>
+            <div className="flex flex-col space-y-2">
+              <label>Bedrooms</label>
+              <input
+                type="number"
+                value={dataBedrooms}
+                onChange={(e) => setDataBedrooms(e.target.value)}
+                className="w-full p-4 border border-gray-600 rounded-xl"
+              />
+            </div>
+            <div className="flex flex-col space-y-2">
+              <label>Bathrooms</label>
+              <input
+                type="number"
+                value={dataBathrooms}
+                onChange={(e) => setDataBathrooms(e.target.value)}
+                className="w-full p-4 border border-gray-600 rounded-xl"
+              />
+            </div>
+            <div className="flex flex-col space-y-2">
+              <label>Maximum number of guests</label>
+              <input
+                type="number"
+                value={dataGuests}
+                onChange={(e) => setDataGuests(e.target.value)}
+                className="w-full p-4 border border-gray-600 rounded-xl"
+              />
+            </div>
+          </div>
+
+          <CustomButton
+            className="mb-2 bg-black hover:bg-gray-800"
+            label="Previous"
+            onClick={() => setCurrentStep(2)}
+          />
+          <CustomButton label="Next" onClick={() => setCurrentStep(4)} />
+        </>
+      ) : currentStep == 4 ? (
+        <>
+          <h2 className="mb-6 text-2xl">Location</h2>
+
+          <div className="pt-3 pb-6 space-y-4">
+            <SelectCountry
+              value={dataCountry}
+              onChange={(value) => setDataCountry(value as SelectCountryValue)}
+            />
+          </div>
+
+          <CustomButton
+            className="mb-2 bg-black hover:bg-gray-800"
+            label="Previous"
+            onClick={() => setCurrentStep(3)}
+          />
+          <CustomButton label="Next" onClick={() => setCurrentStep(5)} />
+        </>
       ) : (
-        <p>Yo</p>
+        <>
+          <h2 className="mb-6 text-2xl">Image</h2>
+
+          <div className="pt-3 pb-6 space-y-4">
+            <div className="py-4 px-6 bg-gray-600 text-white rounded-xl">
+              <input type="file" accept="image/*" onChange={setImage} />
+            </div>
+
+            {dataImage && (
+              <div className="w-[200px] h-[150px] relative">
+                <Image
+                  fill
+                  alt="Uploaded Image"
+                  src={URL.createObjectURL(dataImage)}
+                  className="w-full h-full object-cover rounded-xl"
+                />
+              </div>
+            )}
+          </div>
+
+          <CustomButton
+            className="mb-2 bg-black hover:bg-gray-800"
+            label="Previous"
+            onClick={() => setCurrentStep(4)}
+          />
+          <CustomButton label="Submit" onClick={() => console.log("submit")} />
+        </>
       )}
     </>
   );
