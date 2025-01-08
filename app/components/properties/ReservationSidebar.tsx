09fb26ad1f3f3ceacbb88a35b3291609d61f5203
powtzulist <delasanarwin@gmail.com>
2025-01-08 19:23:14 +0800

    [ui]&[api] reservations ui & api to backend

--- app/components/properties/ReservationSidebar.tsx
+++ app/components/properties/ReservationSidebar.tsx
@@ -2,10 +2,11 @@
 
 import { useState, useEffect } from "react";
 import { Range } from "react-date-range";
-import { differenceInDays, eachDayOfInterval } from "date-fns";
+import { differenceInDays, eachDayOfInterval, format } from "date-fns";
 
 import { apiService } from "@/app/services/apiService";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import { DatePicker } from "../forms/Calendar";
 
 const initialDateRange = {
   startDate: new Date(),
@@ -35,13 +36,77 @@ export const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
   const [totalPrice, setTotalPrice] = useState<number>(0);
   const [guests, setGuests] = useState<string>("1");
   const [dateRange, setDateRange] = useState<Range>(initialDateRange);
-  const [minDate, setMinDate] = useState<Date>(new Date());
+  const [bookedDates, setBookedDates] = useState<Date[]>([]);
   const guestsRange = Array.from(
     { length: property.guests },
     (_, index) => index + 1
   );
 
+  const performBooking = async () => {
+    console.log("performing-book");
+    if (userId) {
+      if (dateRange.startDate && dateRange.endDate) {
+        const formData = new FormData();
+        formData.append("guests", guests);
+        formData.append(
+          "start_date",
+          format(dateRange.startDate, "yyyy-MM-dd")
+        );
+        formData.append("end_date", format(dateRange.endDate, "yyyy-MM-dd"));
+        formData.append("number_of_nights", nights.toString());
+        formData.append("total_price", totalPrice.toString());
+
+        const response = await apiService.post(
+          `/api/properties/${property.id}/book/`,
+          formData
+        );
+
+        if (response.success) {
+          console.log("skibidi success");
+        } else {
+          console.log("something went wrong...");
+        }
+      }
+    } else {
+      loginModal.open();
+    }
+  };
+
+  const _setDateRange = (selection: any) => {
+    const newStartDate = new Date(selection.startDate);
+    const newEndDate = new Date(selection.endDate);
+
+    if (newEndDate <= newStartDate) {
+      newEndDate.setDate(newStartDate.getDate() + 1);
+    }
+
+    setDateRange({
+      ...dateRange,
+      startDate: newStartDate,
+      endDate: newEndDate,
+    });
+  };
+
+  const getReservations = async () => {
+    const reservations = await apiService.get(
+      `/api/properties/${property.id}/reservations`
+    );
+
+    let dates: Date[] = [];
+    reservations.forEach((reservations: any) => {
+      const range = eachDayOfInterval({
+        start: new Date(reservations.start_date),
+        end: new Date(reservations.end_date),
+      });
+
+      dates = [...dates, ...range];
+    });
+
+    setBookedDates(dates);
+  };
+
   useEffect(() => {
+    getReservations(); 
     if (dateRange.startDate && dateRange.endDate) {
       const dayCount = differenceInDays(dateRange.endDate, dateRange.startDate);
 
@@ -63,6 +128,12 @@ export const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
     <aside className="mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl ">
       <h2 className="mb-5 text-2xl">${property.price_per_night} per night</h2>
 
+      <DatePicker
+        value={dateRange}
+        bookedDates={bookedDates}
+        onChange={(value) => _setDateRange(value.selection)}
+      />
+
       <div className="mb-6 p-3 border border-gray-400 rounded-xl">
         <label className="mb-2 block font-bold text-xs ">Guests</label>
         <select
@@ -78,7 +149,10 @@ export const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
         </select>
       </div>
 
-      <div className="w-full mb-6 py-6 text-center text-white bg-airbnb rounded-xl">
+      <div
+        onClick={performBooking}
+        className="w-full mb-6 py-6 text-center text-white bg-airbnb rounded-xl"
+      >
         Book
       </div>
 
