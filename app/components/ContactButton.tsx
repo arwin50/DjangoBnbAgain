4d6465bcfe34ce83d888c4560acf9799d93f7d4c
powtzulist <delasanarwin@gmail.com>
2025-01-10 01:06:57 +0800

    [setup] start conversations functionality

--- app/components/ContactButton.tsx
+++ app/components/ContactButton.tsx
@@ -1,6 +1,39 @@
-export const ContactButton = () => {
+"use client";
+import useLoginModal from "../hooks/useLoginModal";
+import { useRouter } from "next/navigation";
+import { apiService } from "../services/apiService";
+
+interface ContactButtonProps {
+  userId: string | null;
+  landlordId: string;
+}
+
+export const ContactButton: React.FC<ContactButtonProps> = ({
+  userId,
+  landlordId,
+}) => {
+  const loginModal = useLoginModal();
+  const router = useRouter();
+
+  const startConversation = async () => {
+    if (userId) {
+      const conversation = await apiService.get(
+        `/api/chat/start/${landlordId}/`
+      );
+
+      if (conversation.conversation_id) {
+        router.push(`/inbox/${conversation.conversation_id}`);
+      }
+    } else {
+      loginModal.open();
+    }
+  };
+
   return (
-    <div className="mt-6 cursor-pointer py-4 px-6 bg-airbnb hover:bg-airbnb-dark transition text-white rounded-xl">
+    <div
+      onClick={startConversation}
+      className="mt-6 cursor-pointer py-4 px-6 bg-airbnb hover:bg-airbnb-dark transition text-white rounded-xl"
+    >
       Contact
     </div>
   );
