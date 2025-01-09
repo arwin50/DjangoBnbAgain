4efef06d2039f7b5ad0e76d223fc9a5e807f1f51
powtzulist <delasanarwin@gmail.com>
2025-01-10 00:36:58 +0800

    [setup] send and receive messages

--- app/components/inbox/ConversationDetail.tsx
+++ app/components/inbox/ConversationDetail.tsx
@@ -1,8 +1,10 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { CustomButton } from "../forms/CustomButton";
 import { ConversationType } from "@/app/inbox/page";
 import useWebSocket, { ReadyState } from "react-use-websocket";
+import { MessageType } from "@/app/inbox/[id]/page";
+import { UserType } from "@/app/inbox/page";
 
 interface ConversationDetailProps {
   conversation: ConversationType;
@@ -15,8 +17,11 @@ export const ConversationDetail: React.FC<ConversationDetailProps> = ({
   token,
   userId,
 }) => {
+  const messagesDiv = useRef(null);
+  const [newMessage, setNewMessage] = useState("");
   const myUser = conversation.users?.find((user) => user.id == userId);
   const otherUser = conversation.users?.find((user) => user.id != userId);
+  const [realtimeMessages, setRealtimeMessages] = useState<MessageType[]>([]);
 
   const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
     `ws://127.0.0.1:8000/ws/${conversation.id}/?token=${token}`,
@@ -27,17 +32,65 @@ export const ConversationDetail: React.FC<ConversationDetailProps> = ({
     console.log("Connection state changed", readyState);
   }, [readyState]);
 
+  useEffect(() => {
+    if (
+      lastJsonMessage &&
+      typeof lastJsonMessage === "object" &&
+      "name" in lastJsonMessage &&
+      "body" in lastJsonMessage
+    ) {
+      const message: MessageType = {
+        id: "",
+        name: lastJsonMessage.name as string,
+        body: lastJsonMessage.body as string,
+        sent_to: otherUser as UserType,
+        created_by: myUser as UserType,
+        conversationId: conversation.id,
+      };
+
+      setRealtimeMessages((realtimeMessages) => [...realtimeMessages, message]);
+    }
+
+    scrollToBottom();
+  }, [lastJsonMessage]);
+
+  const sendMessage = async () => {
+    sendJsonMessage({
+      event: "chat_message",
+      data: {
+        body: newMessage,
+        name: myUser?.name,
+        sent_to_id: otherUser?.id,
+        conversation_id: conversation.id,
+      },
+    });
+    setNewMessage("");
+    setTimeout(() => {
+      scrollToBottom();
+    }, 50);
+  };
+
+  const scrollToBottom = () => {
+    if (messagesDiv.current) {
+      messagesDiv.current.scrollTop = messagesDiv.current.scrollHeight;
+    }
+  };
+
   return (
     <div>
-      <div className="max-h-[400px] overflow-auto flex flex-col space-y-4">
-        <div className="w-[80%] py-4 px-6 rounded-xl bg-gray-200">
-          <p className="font-bold text-gray-500">John Doe</p>
-          <p>Skibidi</p>
-        </div>
-        <div className="w-[80%] ml-[20%] py-4 px-6 rounded-xl bg-blue-200">
-          <p className="font-bold text-gray-500">The Rizzler</p>
-          <p>What the Sigma?</p>
-        </div>
+      <div
+        className="max-h-[400px] overflow-auto flex flex-col space-y-4"
+        ref={messagesDiv}
+      >
+        {realtimeMessages.map((message, index) => (
+          <div
+            key={`${message.id}_${index}`}
+            className={`w-[80%] py-4 px-6 rounded-xl ${message.name === myUser?.name ? "ml-[20%] bg-blue-200 " : "bg-gray-200"}`}
+          >
+            <p className="font-bold text-gray-500">{message.name}</p>
+            <p>{message.body}</p>
+          </div>
+        ))}
       </div>
 
       <div className="mt-4 py-4 px-6 flex border border-gray-300 space-x-4 rounded-xl">
@@ -45,12 +98,10 @@ export const ConversationDetail: React.FC<ConversationDetailProps> = ({
           type="text"
           placeholder="Type your mesage..."
           className="w-full p-2 bg-gray-200 rounded-xl"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
         />
-        <CustomButton
-          label="Send"
-          onClick={() => console.log("skibidi")}
-          className="w-[99px]"
-        />
+        <CustomButton label="Send" onClick={sendMessage} className="w-[99px]" />
       </div>
     </div>
   );
